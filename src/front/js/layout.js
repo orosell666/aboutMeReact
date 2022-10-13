8217c8f8bba7fb7134c00af2c569574c489b7d8f
orosell666 <o.rosell.ribas@gmail.com>
2022-10-13 21:04:35 +0000

    scri¡

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -20,7 +20,7 @@ const Layout = () => {
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
-                    <Navbar />
+
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
