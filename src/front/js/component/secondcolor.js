0e9b9d461f52d960d41ffa31178a52997e68bed6
orosell666 <o.rosell.ribas@gmail.com>
2022-11-10 18:10:23 +0000

    mapa

--- src/front/js/component/secondcolor.js
+++ src/front/js/component/secondcolor.js
@@ -1,9 +1,10 @@
 import React from "react";
-
 import "../../styles/home.css";
+import { SimpleMap } from "../component/map";
 
 
 export const SecondColor = () => {
+
     return (
         <div className="secondColor d-inline-flex" id="scrollspyHeading2">
 
@@ -11,6 +12,9 @@ export const SecondColor = () => {
                 <div className="ms-5 titleBox float-start me-5 ">
                     <h1 className="p-5">endtroduction</h1>
                     <p className="mt-4">A"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                </div>
+                <div>
+                    < SimpleMap />
                 </div>
 
             </div>
