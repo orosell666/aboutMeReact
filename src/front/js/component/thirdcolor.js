057a146d5ce0e5c7e646314168cf21a9cb75aeb0
orosell666 <o.rosell.ribas@gmail.com>
2022-11-14 23:07:47 +0000

    text

--- src/front/js/component/thirdcolor.js
+++ src/front/js/component/thirdcolor.js
@@ -7,45 +7,41 @@ import { Navbar } from "../component/navbar";
 
 export const ThirdColor = () => {
     return (
-        <div className="thirdColor d-inline-flex" id="scrollspyHeading3">
+        <div className="thirdColor position-relative" id="scrollspyHeading3">
             <div className="d-inline-flex ">
-                <div className="ms-5  titleBox float-start me-5 ">
-                    <h1 className="p-5">What I've been doing...</h1>
-                    <div><ul className="mt-4 ">
-                        <li>
-                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                        </li>
-                        <li>
-                            <p>Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+                <div className="container-fluid d-inline-flex ">
+                    <div className="ms-5  titleBox float-start me-5 col-11">
+                        <h1 className="p-5">What I've been doing...</h1>
+                        <div><ul className="mt-4 ">
+                            <li>
+                                <p>Project Managing</p>
+                            </li>
+                            <li>
+                                <p>Events
 
-                            </p>
-                        </li>
-                        <li>
-                            <p>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi</p>
-                        </li>
-                        <li>
-                            <p>Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu</p>
-                        </li>
-                        <li>
-                            <p>Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.</p>
-                        </li>
-                        <li>
-                            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                        </li>
-                    </ul></div>
-                </div>
-            </div>
-            <div className="downArrow position-relative top-50 end-0 me-5 ">
-                <ul className="nav nav-pills">
-                    <li className=" nav-item">
-                        <a className="arrowdark bi bi-chevron-up" href="#scrollspyHeading2"></a>
-                    </li>
-                    <li className="nav-item mt-4">
-                        <a className="arrowdark bi bi-chevron-down" href="#scrollspyHeading4"></a>
-                    </li>
-                </ul>
+                                </p>
+                            </li>
+                            <li>
+                                <p>Snowboarding</p>
+                            </li>
+                            <li>
+                                <p>Production</p>
+                            </li>
+                        </ul></div>
+                    </div>
 
-            </div>
+                    <div className="downArrow position-relative top-50 end-0 me-5 ">
+                        <ul className="nav nav-pills">
+                            <li className=" nav-item">
+                                <a className="arrowdark bi bi-chevron-up" href="#scrollspyHeading2"></a>
+                            </li>
+                            <li className="nav-item mt-4">
+                                <a className="arrowdark bi bi-chevron-down" href="#scrollspyHeading4"></a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div></div>
         </div>
     )
 }
